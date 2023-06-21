78de78c9540b45c121968a980386ae6377ed2ec1
SRR23 <srr267@gmail.com>
2023-06-21 23:10:39 +0600

    added

---
+++ Week_3/Day_6/Codeforces_1800A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+            {
+                s[i] += 32;
+            }
+        }
+        s.erase(unique(s.begin(), s.end()), s.end());
+        if (s == "meow")
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+}
