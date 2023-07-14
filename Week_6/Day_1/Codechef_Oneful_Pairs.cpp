84b4d378ce91c948d320025d7223367bbf8be659
SRR23 <srr267@gmail.com>
2023-07-14 17:29:00 +0600

    added

---
+++ Week_6/Day_1/Codechef_Oneful_Pairs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    if (((a + b) + (a * b)) == 111)
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+}
