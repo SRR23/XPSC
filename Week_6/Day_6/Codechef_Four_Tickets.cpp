cd91109e35912f13081d4c2819726315460cf06d
SRR23 <srr267@gmail.com>
2023-07-19 23:00:11 +0600

    added

---
+++ Week_6/Day_6/Codechef_Four_Tickets.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if ((n * 4) <= 1000)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+}
