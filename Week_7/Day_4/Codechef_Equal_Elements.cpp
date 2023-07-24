ff1167ca499bdb01684ed7a51569fe9981ab46b8
SRR23 <srr267@gmail.com>
2023-07-24 11:15:24 +0600

    added

---
+++ Week_7/Day_4/Codechef_Equal_Elements.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            mp[a[i]]++;
+        }
+        int mx = 0;
+        for (auto it : mp)
+        {
+            mx = max(mx, it.second);
+        }
+        cout << n - mx << '\n';
+    }
+}
